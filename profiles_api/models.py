2e67666bc4e684fe9a417040ee9f4ebcf183ee40
Woosang <yws1502@naver.com>
2021-09-23 21:43:59 +0900

    Added helloApiView

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -31,7 +31,6 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
